888253df63aee1ae9aae147ef1a47802a891c435
jwgbradford <jwgbradford@gmail>
2021-03-26 09:29:43 +0000

    update

--- chess_main.py
+++ chess_main.py
@@ -4,3 +4,9 @@ pygame.init()
 #set up board size
 TILE_SIZE = 100
 TILES = 8
+OFFSET = 25
+BOARD_SIZE = (TILE_SIZE * TILES) + (OFFSET * 2)
+BLUE_IMAGE = pygame.Surface((TILE_SIZE,TILE_SIZE))
+BLUE_IMAGE.fill(pygame.Color('lightskyblue2'))
+GRAY_IMAGE = pygame.Surface((TILE_SIZE,TILE_SIZE))
+GRAY_IMAGE.fill(pygame.Color('slategray4'))
