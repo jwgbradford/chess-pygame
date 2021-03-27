5e9444d6a17c19b72c9d5c7c03d47c305f518bf0
jwgbradford <jwgbradford@gmail>
2021-03-27 10:04:39 +0000

    marking out added

--- chess_board.py
+++ chess_board.py
@@ -334,8 +334,8 @@ BLUE_IMAGE = pygame.Surface((TILE_SIZE,TILE_SIZE))
 BLUE_IMAGE.fill(pygame.Color('lightskyblue2'))
 GRAY_IMAGE = pygame.Surface((TILE_SIZE,TILE_SIZE))
 GRAY_IMAGE.fill(pygame.Color('slategray4'))
-FONT = pygame.font.Font(None, 32)
 
+FONT = pygame.font.Font(None, 32)
 TEXT_COLOUR = pygame.Color('darkslategrey')
 COLUMNS = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 ROWS = [ '1', '2', '3', '4', '5', '6', '7', '8']
--- chess_main-pg5.py
+++ chess_main-pg5.py
@@ -19,22 +19,6 @@ class Board():
                 # Blit one image after the other at their respective coords
                 background.blit(image, ((row * TILE_SIZE) + OFFSET, (column * TILE_SIZE) + OFFSET))
             next(images)
-        background = self.mark_out(background)
-        return background
-
-    def mark_out(self, background):
-        for row, row_lable in enumerate(ROWS):
-            txt_image = FONT.render(row_lable, True, TEXT_COLOUR)
-            # labels up & down the left
-            background.blit(txt_image, (5, (row * TILE_SIZE) + (OFFSET * 3)))
-            # labels up and down the right, we use the same txt_image
-            background.blit(txt_image, (BOARD_SIZE - 20, (row * TILE_SIZE) + (OFFSET * 3)))
-        for column, column_lable in enumerate(COLUMNS):
-            txt_image = FONT.render(column_lable, True, TEXT_COLOUR)
-            # labels along the top
-            background.blit(txt_image, ((column * TILE_SIZE) + (OFFSET * 3), 5))
-            # labels along the bottom, we use the same txt_image
-            background.blit(txt_image, ((column * TILE_SIZE) + (OFFSET * 3), BOARD_SIZE - 20))
         return background
 
     def run(self):
@@ -52,10 +36,6 @@ BLUE_IMAGE = pygame.Surface((TILE_SIZE,TILE_SIZE))
 BLUE_IMAGE.fill(pygame.Color('lightskyblue2'))
 GRAY_IMAGE = pygame.Surface((TILE_SIZE,TILE_SIZE))
 GRAY_IMAGE.fill(pygame.Color('slategray4'))
-FONT = pygame.font.Font(None, 32)
-TEXT_COLOUR = pygame.Color('darkslategrey')
-COLUMNS = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-ROWS = [ '1', '2', '3', '4', '5', '6', '7', '8']
 
 if __name__ == '__main__':
     my_chess_game = Board()
